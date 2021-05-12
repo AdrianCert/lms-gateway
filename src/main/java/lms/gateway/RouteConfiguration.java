277b29bfc9e3619726dc676c3e056eba45be80ca
Cezar Lupu <cezarlupu2012@gmail.com>
2021-05-13 00:09:39 +0300

    update cu task grader

--- src/main/java/lms/gateway/RouteConfiguration.java
+++ src/main/java/lms/gateway/RouteConfiguration.java
@@ -7,7 +7,8 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RouteConfiguration {
-    private static final String API_TBD = "https://tbd-dev.herokuapp.com/";
+    private static final String API_TASK_HANDLER = "https://tbd-dev.herokuapp.com/";
+    private static final String API_TASK_GRADER = "https://tbd2-dev.herokuapp.com/";
     private static final String API_FORUM_MS = "https://lms-forum-app.herokuapp.com/";
     private static final String API_MAIL_MS = "https://mail-microservice.herokuapp.com/";
 
@@ -17,22 +18,22 @@ public class RouteConfiguration {
         return builder.routes()
                 //TASK-HANDLER
                 .route(r -> r.path("/task/**")
-                        .uri(API_TBD))
+                        .uri(API_TASK_HANDLER))
                 .route(r -> r.path("/professor/**")
-                        .uri(API_TBD))
+                        .uri(API_TASK_HANDLER))
                 .route(r -> r.path("/question/**")
-                        .uri(API_TBD))
+                        .uri(API_TASK_HANDLER))
                 .route(r -> r.path("/task_professor/**")
-                        .uri(API_TBD))
+                        .uri(API_TASK_HANDLER))
                 .route(r -> r.path("/task_question/**")
-                        .uri(API_TBD))
+                        .uri(API_TASK_HANDLER))
                 //TASK-GRADER
                 .route(r -> r.path("/sheet/**")
-                        .uri(API_TBD))
+                        .uri(API_TASK_GRADER))
                 .route(r -> r.path("/sheet_task/**")
-                        .uri(API_TBD))
+                        .uri(API_TASK_GRADER))
                 .route(r -> r.path("/sheet_student/**")
-                        .uri(API_TBD))
+                        .uri(API_TASK_GRADER))
                 //trebuie adaugata partea de grading - inca pe localhost
 
                 //TASK-FORUM
