f0aedcb1a099f08d646a8dc6090a5d2bf9d79f68
Cezar Lupu <cezarlupu2012@gmail.com>
2021-05-14 00:30:23 +0300

    update for CORS

--- src/main/java/lms/gateway/GatewayApplication.java
+++ src/main/java/lms/gateway/GatewayApplication.java
@@ -13,17 +13,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @SpringBootApplication
 @EnableEurekaClient
 public class GatewayApplication {
-//nou
-	@Bean
-	public WebMvcConfigurer configure(){
-		return new WebMvcConfigurer(){
-			@Override
-			public void addCorsMappings(CorsRegistry registry){
-				registry.addMapping("/*").allowedOrigins("http://localhost:4200");
-			}
-		};
-	}
-//ce era deja
+////nou
+//	@Bean
+//	public WebMvcConfigurer configure(){
+//		return new WebMvcConfigurer(){
+//			@Override
+//			public void addCorsMappings(CorsRegistry registry){
+//				registry.addMapping("/*").allowedOrigins("http://localhost:4200");
+//			}
+//		};
+//	}
+////ce era deja
     public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
