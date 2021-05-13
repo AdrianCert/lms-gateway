bacf61131f16509a074a5027f4ca7479580de3f4
Cezar Lupu <cezarlupu2012@gmail.com>
2021-05-13 21:23:23 +0300

    trying to solve cors - Cezar

--- src/main/java/lms/gateway/GatewayApplication.java
+++ src/main/java/lms/gateway/GatewayApplication.java
@@ -3,12 +3,27 @@ package lms.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @SpringBootApplication
 @EnableEurekaClient
 public class GatewayApplication {
+//nou
+	@Bean
+	public WebMvcConfigurer configure(){
+		return new WebMvcConfigurer(){
+			@Override
+			public void addCorsMappings(CorsRegistry registry){
+				registry.addMapping("/*").allowedOrigins("http://localhost:4200");
+			}
+		};
+	}
+//ce era deja
     public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
