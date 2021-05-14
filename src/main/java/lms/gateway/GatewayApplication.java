7ae29a7971fff9ceaa96d6312763312ac6bdd6c5
Cezar Lupu <cezarlupu2012@gmail.com>
2021-05-14 18:57:53 +0300

    Update GatewayApplication.java

--- src/main/java/lms/gateway/GatewayApplication.java
+++ src/main/java/lms/gateway/GatewayApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 
 
 import java.util.Arrays;
-
+@CrossOrigin(origins = "http://localhost:4200")
 @Configuration
 @SpringBootApplication
 @EnableEurekaClient
