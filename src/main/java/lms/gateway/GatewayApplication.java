fd11490e73d228ecf5a13db216b94346d22da963
Cezar Lupu <cezarlupu2012@gmail.com>
2021-05-14 18:21:29 +0300

    back - doesn't work

--- src/main/java/lms/gateway/GatewayApplication.java
+++ src/main/java/lms/gateway/GatewayApplication.java
@@ -3,14 +3,8 @@ package lms.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 import java.util.Arrays;
 
@@ -30,18 +24,6 @@ public class GatewayApplication {
 //	}
 ////ce era deja
 
-	@Bean
-	CorsConfigurationSource corsConfigurationSource() {
-		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.setAllowedOrigins(Arrays.asList("*"));
-		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS"));
-		configuration.setAllowedHeaders(Arrays.asList("Access-Control-Allow-Headers", "Access-Control-Allow-Origin",
-				"Access-Control-Request-Method", "Access-Control-Request-Headers", "Origin",
-				"Cache-Control", "Content-Type"));
-		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**", configuration);
-		return source;
-	}
 
     public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
