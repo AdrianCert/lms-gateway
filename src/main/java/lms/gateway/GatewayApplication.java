08dcb3bb692c59f272f9c0494b1d620c8babbe21
Cezar Lupu <cezarlupu2012@gmail.com>
2021-05-14 12:09:50 +0300

    -dependenta webmvc

--- src/main/java/lms/gateway/GatewayApplication.java
+++ src/main/java/lms/gateway/GatewayApplication.java
@@ -3,14 +3,7 @@ package lms.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @SpringBootApplication
