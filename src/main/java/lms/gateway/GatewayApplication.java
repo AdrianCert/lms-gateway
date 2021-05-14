f7128acfe0e9ce09dd910144d73174638721d5f1
Cezar Lupu <cezarlupu2012@gmail.com>
2021-05-14 19:27:52 +0300

    app yml

--- src/main/java/lms/gateway/GatewayApplication.java
+++ src/main/java/lms/gateway/GatewayApplication.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @SpringBootApplication
 @EnableEurekaClient
 @CrossOrigin("http://localhost:4200")
+
 public class GatewayApplication {
 ////nou
 //	@Bean
