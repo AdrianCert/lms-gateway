2b1ea750b81f7063c6ac849dac64b82b8c6b72f4
Cezar Lupu <cezarlupu2012@gmail.com>
2021-05-17 21:31:43 +0300

    try to Zuul It

--- src/main/java/lms/gateway/GatewayApplication.java
+++ src/main/java/lms/gateway/GatewayApplication.java
@@ -1,31 +1,28 @@
 package lms.gateway;
 
+import lms.gateway.filters.SimpleFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Configuration
+@EnableZuulProxy
 @SpringBootApplication
 @EnableEurekaClient
 @CrossOrigin("http://localhost:4200")
 
 public class GatewayApplication {
-////nou
-//	@Bean
-//	public WebMvcConfigurer configure(){
-//		return new WebMvcConfigurer(){
-//			@Override
-//			public void addCorsMappings(CorsRegistry registry){
-//				registry.addMapping("/*").allowedOrigins("http://localhost:4200");
-//			}
-//		};
-//	}
-////ce era deja
-
 
     public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+	@Bean
+	public SimpleFilter simpleFilter() {
+		return new SimpleFilter();
 	}
 }
