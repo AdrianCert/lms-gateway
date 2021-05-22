7f03499c534c7baf4012b70a6769fd45b191f77d
Adrian Panaintescu <adrian.cert@gmail.com>
2021-05-22 22:43:27 +0300

    update

--- src/main/java/lms/gateway/GatewayApplication.java
+++ src/main/java/lms/gateway/GatewayApplication.java
@@ -10,21 +10,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @SpringBootApplication
 @EnableEurekaClient
 @CrossOrigin("http://localhost:4200")
-
 public class GatewayApplication {
-////nou
-//	@Bean
-//	public WebMvcConfigurer configure(){
-//		return new WebMvcConfigurer(){
-//			@Override
-//			public void addCorsMappings(CorsRegistry registry){
-//				registry.addMapping("/*").allowedOrigins("http://localhost:4200");
-//			}
-//		};
-//	}
-////ce era deja
-
-
     public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
